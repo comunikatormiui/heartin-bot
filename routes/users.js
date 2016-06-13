b7033f6440dfbdcea6ce8cdcb74bd99ebd214bc7
Sergey Ovechkin <me@pomeo.me>
2016-06-13 11:05:03 +0300

    convert express router users to es2015

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,3 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+export default (req, res) => {
   res.send('respond with a resource');
-});
-
-module.exports = router;
+};
