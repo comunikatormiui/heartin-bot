06f2eb25c438d2a79d5c2f6c9b466b694ec59308
Sergey Ovechkin <me@pomeo.me>
2016-06-14 17:36:40 +0300

    add file with links to pages with pdf files

---
+++ config/links.js
@@ -0,0 +1,14 @@
+const links = [
+  'http://eurheartj.oxfordjournals.org/content/early/2016/05/23/eurheartj.ehw106', // CVD Prevention in clinical practice (European Guidelines on)
+  'http://eurheartj.oxfordjournals.org/content/early/2016/05/19/eurheartj.ehw128', // Acute and Chronic Heart Failure
+  'http://eurheartj.oxfordjournals.org/content/36/44/3075', // Infective Endocarditis (Guidelines on Prevention, Diagnosis and Treatment of)
+  'http://eurheartj.oxfordjournals.org/content/36/41/2793', // Ventricular Arrhythmias and the Prevention of Sudden Cardiac Death
+  'http://eurheartj.oxfordjournals.org/content/36/42/2921', // Pericardial Diseases (Guidelines on the Diagnosis and Management of)
+  'http://eurheartj.oxfordjournals.org/content/37/3/267', // Acute Coronary Syndromes (ACS) in patients presenting without persistent ST-segment elevation (Management of)
+  'http://eurheartj.oxfordjournals.org/content/37/1/67', // Pulmonary Hypertension (Guidelines on Diagnosis and Treatment of)
+  'http://eurheartj.oxfordjournals.org/content/early/2014/08/28/eurheartj.ehu284', // Hypertrophic Cardiomyopathy
+  'http://eurheartj.oxfordjournals.org/content/early/2014/08/28/eurheartj.ehu281', // Aortic Diseases
+  'http://eurheartj.oxfordjournals.org/content/early/2014/08/28/eurheartj.ehu278' // ESC/EACTS Guidelines on Myocardial Revascularisation
+];
+
+export default links;
