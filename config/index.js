887462c715acdb22bab8bfce4a68d53468654bd3
Sergey Ovechkin <me@pomeo.me>
2016-06-14 18:09:03 +0300

    add environment variable for token

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const config = {
   mongoURL: process.env.MONGO_URL || 'mongodb://mac5.ixcglobal.com:27017/heartin-bot',
   port: process.env.PORT || 3978,
-  token: 'EAANoqHvZBBckBAPS5td0KpIatcKLEL2jnflMXQTdm3XbXiP73eAnFVbBIBZAADQH8RSNwK3IoEELa4q1wkq2ZBXA7eVBHRmX25huGRRJr0NLF99NdhVAg2MKXtSTRJWs3vSmCiTZBl8qgV6RzaC4Dt7L98jy2ujZCZCOamaw1rtAZDZD'
+  token: process.env.TOKEN || 'EAANoqHvZBBckBAPS5td0KpIatcKLEL2jnflMXQTdm3XbXiP73eAnFVbBIBZAADQH8RSNwK3IoEELa4q1wkq2ZBXA7eVBHRmX25huGRRJr0NLF99NdhVAg2MKXtSTRJWs3vSmCiTZBl8qgV6RzaC4Dt7L98jy2ujZCZCOamaw1rtAZDZD'
 };
 
 export default config;
