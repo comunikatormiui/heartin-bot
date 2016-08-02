64ad19d392221239ede30b8240346c87fec9b24e
Sergey Ovechkin <me@pomeo.me>
2016-08-02 18:59:38 +0300

    options for parser

---
+++ config/parse.js
@@ -0,0 +1,31 @@
+const config = {
+  colors: ['e0f0f0', 'f2f2e2', 'f2f2e3', 'f2f2e4'],
+  options: [
+    {
+      name: 'font-size',
+      value: '60.000400px'
+    },
+    {
+      name: 'font-size',
+      value: '49.999600px'
+    },
+    {
+      name: 'font-family',
+      value: 'ff1'
+    },
+    {
+      name: 'transform',
+      value: 'matrix(0.343054,0.000000,0.000000,0.304901,0,0)'
+    },
+    {
+      name: 'font-size',
+      value: '40.000400px'
+    },
+    {
+      name: 'font-family',
+      value: 'ffd'
+    }
+  ]
+};
+
+export default config;
