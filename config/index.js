496f53bf08a61b0807c9c7a666f88ce5bcfda108
Oleksii Vynogradov <alex@mac5.ixcglobal.com>
2016-09-16 23:07:48 +0300

    Changed mongodb address to localhost

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://mac5.ixcglobal.com:27017/heartin-bot',
+  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/heartin-bot',
   port: process.env.PORT || 3978,
   token: process.env.TOKEN || 'EAANoqHvZBBckBAPS5td0KpIatcKLEL2jnflMXQTdm3XbXiP73eAnFVbBIBZAADQH8RSNwK3IoEELa4q1wkq2ZBXA7eVBHRmX25huGRRJr0NLF99NdhVAg2MKXtSTRJWs3vSmCiTZBl8qgV6RzaC4Dt7L98jy2ujZCZCOamaw1rtAZDZD',
   appId: 'heartin',
